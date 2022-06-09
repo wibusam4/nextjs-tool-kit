72af93cb2a12104ffb7029385fddcb6063456182
Zen0Sama <vtp2104@gmail.com>
2022-06-09 17:14:41 +0700

    update redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import { Provider } from 'react-redux'
+import store from '../redux/store'
+import Head from 'next/head'
+import { ToastContainer } from 'react-toastify'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Provider store={store}>
+                <ToastContainer />
+                <Head>
+                    <title>NextJs Tool Kit</title>
+                    <meta name="description" content="WibuSama" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <Component {...pageProps} />
+            </Provider>
+        </>
+    )
 }
 
 export default MyApp
